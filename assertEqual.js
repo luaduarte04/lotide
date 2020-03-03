55eb666c713ee49bb3615e91b5c8da99f22e4609
Luana Duarte <lua.duarte04@gmail.com>
2020-03-03 14:37:56 +0000

    Personal assert function

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  //compare the actual and expected values
+  if (actual === expected) {
+    //if they match, show in console assertion passed....
+    console.log(`✅✅✅ Assertion Failed: ${actual} === ${expected}`);
+  } else {
+    //if they dont match, show in console assertion failed
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
