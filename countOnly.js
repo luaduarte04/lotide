b0de112d69e3c4b9be77c90c06fda329aa403a44
Luana Duarte <lua.duarte04@gmail.com>
2020-03-04 18:38:00 +0000

    comparing a string to an object and returning a new object with same keys

---
+++ countOnly.js
@@ -0,0 +1,46 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  //compare the actual and expected values
+  if (actual === expected) {
+    //if they match, show in console assertion passed....
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    //if they dont match, show in console assertion failed
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
