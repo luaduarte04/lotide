2af0d7457c5f27bc06a3bbc4b1815c700e729da6
Luana Duarte <lua.duarte04@gmail.com>
2020-03-09 18:50:32 +0000

    Refactor code

--- assertEqual.js
+++ assertEqual.js
@@ -3,7 +3,7 @@ const assertEqual = function(actual, expected) {
   //compare the actual and expected values
   if (actual === expected) {
     //if they match, show in console assertion passed....
-    console.log(`✅✅✅ Assertion Failed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     //if they dont match, show in console assertion failed
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
@@ -11,6 +11,7 @@ const assertEqual = function(actual, expected) {
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
 
+module.exports = assertEqual;
